074549ebd306b550165a839608ff16730d892e5d
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2025-01-15 15:08:10 +0100

    refonte visuel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,13 +1,8 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController {
